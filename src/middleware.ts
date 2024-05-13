745aa671a14ee11c5511e44c12ca71496dc19713
iinava <131179814+iinava@users.noreply.github.com>
2024-05-13 20:14:32 +0530

    debugging

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,19 +1,29 @@
-import {
-  clerkMiddleware,
-  createRouteMatcher
-} from '@clerk/nextjs/server';
+// import {
+//   clerkMiddleware,
+//   createRouteMatcher
+// } from '@clerk/nextjs/server';
 
-const isProtectedRoute = createRouteMatcher([
-  '/dashboard(.*)',
-  '/forum(.*)',
+// const isProtectedRoute = createRouteMatcher([
+//   '/dashboard(.*)',
 
-]);
+// ]);
 
 
-export default clerkMiddleware((auth, req) => {
-  if (isProtectedRoute(req)) auth().protect();
-});
+// export default clerkMiddleware((auth, req) => {
+//   if (isProtectedRoute(req)) auth().protect();
+  
+// });
+
+// export const config = {
+//   matcher: ['/((?!.*\\..*|_next).*)', '/', '/(api|trpc)(.*)'],
+// };
+
+
+import { clerkMiddleware } from '@clerk/nextjs/server';
+
+// Make sure that the `/api/webhooks/(.*)` route is not protected here
+export default clerkMiddleware()
 
 export const config = {
-  matcher: ['/((?!.*\\..*|_next).*)', '/', '/(api|trpc)(.*)'],
+  matcher: [ '/((?!.*\\..*|_next).*)', '/', '/(api|trpc)(.*)'],
 };
